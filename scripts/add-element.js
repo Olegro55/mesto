f26eb941bd6ce26c7ce43af57e089bb1f07678ab
Olegro55 <120279592+Olegro55@users.noreply.github.com>
2023-03-02 15:43:48 +0300

    Add files via upload

---
+++ scripts/add-element.js
@@ -0,0 +1,19 @@
+const formAddElement = document.querySelector('#add-element');
+const placeInput = document.querySelector('.popup__item_type_place');
+const elementFotoLink = document.querySelector('.element__foto');
+const fotoLinkInput = document.querySelector('.popup__item_type_photo');
+const elementAddButton = document.querySelector('.profile__add-button');
+const elementCloseIcon = document.querySelector('#close-icon');
+const formsElement = document.querySelector('#form');
+
+elementAddButton.addEventListener('click', openFormElement);
+
+elementCloseIcon.addEventListener('click', closeFormElement);
+
+function openFormElement() {
+  formAddElement.classList.add('popup_opened');
+}
+
+function closeFormElement() {
+  formAddElement.classList.remove('popup_opened');
+}
