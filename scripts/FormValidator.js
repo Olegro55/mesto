583a7ca17521e01ca104d1427bbbeadf9e3c10d8
Olegro55 <120279592+Olegro55@users.noreply.github.com>
2023-04-01 18:11:26 +0300

    Add files via upload

---
+++ scripts/FormValidator.js
@@ -0,0 +1,74 @@
+export class FormValidator {
+  constructor(config, formElement) {
+    this._form = formElement;
+    this._inputList = Array.from(this._form.querySelectorAll(config.inputSelector));
+    this._button = this._form.querySelector(config.submitButtonSelector);
+
+    this._inactiveButtonClass = config.inactiveButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+  }
+
+  enableValidation() {
+    this._toggleButtonState();
+    this._setEventListeners();
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const activeError = this._form.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(this._inputErrorClass);
+    activeError.classList.add(this._errorClass);
+    activeError.textContent = errorMessage;
+  }
+
+  _hideInputError(inputElement) {
+    const activeError = this._form.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    activeError.classList.remove(this._errorClass);
+    activeError.textContent = '';
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some(inputElement => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _disableButton() {
+    this._button.classList.add(this._inactiveButtonClass);
+    this._button.disabled = true;
+  }
+
+  _enableButton() {
+    this._button.classList.remove(this._inactiveButtonClass);
+    this._button.disabled = false;
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._disableButton();
+    } else {
+      this._enableButton();
+    }
+  }
+
+  _setEventListeners() {
+    this._form.addEventListener('input', () => {
+      this._toggleButtonState();
+    });
+
+    this._inputList.forEach(inputElement => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+      });
+    });
+  }
+}
---
+++ scripts/constants.js
@@ -0,0 +1,26 @@
+const cards = [
+  {
+    name: 'Карачаевск',
+    link: 'https://olegro55.github.io/mesto/images/karachaevsk.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: 'https://olegro55.github.io/mesto/images/elbrus.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: 'https://olegro55.github.io/mesto/images/dombay.jpg'
+  },
+  {
+    name: 'Красноярские столбы',
+    link: 'https://olegro55.github.io/mesto/images/stolby.jpg'
+  },
+  {
+    name: 'Казань',
+    link: 'https://olegro55.github.io/mesto/images/kazan.jpg'
+  },
+  {
+    name: 'Плато Путорана',
+    link: 'https://olegro55.github.io/mesto/images/putoran.jpg'
+  }
+];
